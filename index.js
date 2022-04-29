92a33fb504d3b2a02045c9a0ecfb5fdf0cfe4e1b
ImranHossain5722 <imd572258@gmail.com>
2022-04-29 14:38:27 +0600

    heroku update

--- index.js
+++ index.js
@@ -16,13 +16,20 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bvvu1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-client.connect(err => {
-  const collection = client.db("carSale").collection("product");
-  console.log('mongo is connected ');
-  // perform actions on the collection object
-  client.close();
-});
+async function run (){
 
+    try{
+
+        await client.connect();
+        const productCollection = client.db('car-sale').collection('product')
+
+
+    }
+    finally{
+
+    }
+}
+run().catch{console.dir}
 
 
 app.get ('/', (req,res)=>{
