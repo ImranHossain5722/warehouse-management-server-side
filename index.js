6b521a65e788d64199709aeb8ad6af63b787e88d
ImranHossain5722 <imd572258@gmail.com>
2022-04-30 15:06:16 +0600

    build api data load

--- index.js
+++ index.js
@@ -1,53 +1,57 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const port = process.env.PORT || 5000
-const app = express ()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 //middleware
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 // user name: carSaleUser password: dTHGlX2ejwjbzWUP
 
-// mongodb database 
+// mongodb database
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bvvu1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run (){
+async function run() {
+  try {
+    await client.connect();
+    const inventoryItemsCollection = client
+      .db("car-sale")
+      .collection("inventory-items");
+    
+    // data load
+      app.get("/inventoryItems", async (req, res) => {
+      const query = {};
+      const cursor = inventoryItemsCollection.find(query);
+      const inventoryItems = await cursor.toArray();
+      res.send(inventoryItems)
+    });
 
-    try{
-
-        await client.connect();
-        const productCollection = client.db('car-sale').collection('product')
 
 
-    }
-    finally{
 
-    }
+
+  } finally {
+  }
 }
-run().catch(console.dir)
+run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("assignment project is running");
+});
 
-app.get ('/', (req,res)=>{
+app.get("/hero", (req, res) => {
+  res.send("heroku check ");
+});
 
-    res.send('assignment project is running')
-})
-
-
-app.get('/hero', (req,res)=>{
-
-    res.send('heroku check ')
-})
-
-
-
-
-
-
-app.listen(port,()=>{
-    console.log('assignmetn server work to port ', port);
-})
+app.listen(port, () => {
+  console.log("assignmetn server work to port ", port);
+});
