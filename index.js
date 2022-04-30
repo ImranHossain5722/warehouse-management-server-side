dd7c866049ad3eae9765eee34aa9297d83a42551
ImranHossain5722 <imd572258@gmail.com>
2022-04-30 22:23:11 +0600

    try to buid id API

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 //middleware
 app.use(cors());
@@ -35,7 +35,13 @@ async function run() {
       res.send(inventoryItems)
     });
 
+    app.get('/manageitem/:id',async(req,res)=>{
 
+        const id =req.params.id
+        const query= {_id: ObjectId(id)}
+        const inventoryItem =await inventoryItemsCollection.findOne(query)
+        res.send(inventoryItem)
+    })
 
 
 
