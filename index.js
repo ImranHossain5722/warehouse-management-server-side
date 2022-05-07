9e66bd454145d79a238f5b843bab424ab86d5195
ImranHossain5722 <imd572258@gmail.com>
2022-05-07 23:58:11 +0600

    update to inventroyItems Api for paginations

--- index.js
+++ index.js
@@ -20,124 +20,114 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const inventoryItemsCollection = client .db("car-sale").collection("inventory-items")
-    const blogsCollection =client.db("car-sale").collection("blogs")
-   
-
-    app.get('/inventoryItems/:id', async(req,res)=>{
-
-        const id = req.params.id
-        const query= {_id: ObjectId (id)}
-        const inventoryItem = await inventoryItemsCollection.findOne(query)
-        res.send(inventoryItem)
-    })
+    const inventoryItemsCollection = client.db("car-sale").collection("inventory-items");
+    const blogsCollection = client.db("car-sale").collection("blogs");
+      //single items by id 
+    app.get("/inventoryItems/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const inventoryItem = await inventoryItemsCollection.findOne(query);
+      res.send(inventoryItem);
+    });
 
     //post
-    app.post('/inventoryItems', async (req, res)=>{
-
-      const newItem =req.body;
-      const result = await inventoryItemsCollection.insertOne(newItem)
+    app.post("/inventoryItems", async (req, res) => {
+      const newItem = req.body;
+      const result = await inventoryItemsCollection.insertOne(newItem);
       res.send(result);
-    })
- 
+    });
+
     // data load
-    app.get('/inventoryItems', async (req, res) => {
+    app.get("/inventoryItems", async (req, res) => {
+      const page= parseInt(req.query.page);
+      const size =  parseInt(req.query.size);
       const query = {};
       const cursor = inventoryItemsCollection.find(query);
-      const inventoryItems = await cursor.toArray();
-      res.send(inventoryItems)
+      let inventoryItems ;
+      if( page || size ){
+
+        inventoryItems = await cursor.skip(page*size).limit(size).toArray();
+      }else{
+
+        inventoryItems = await cursor.toArray();
+      }
+      res.send(inventoryItems);
     });
 
     // stock items collections
-    app.get('/inventoryItems'), async (req, res )=>{
-      const email = req.query.email ;
-      const query ={email:email}
-      const cursor = inventoryItemsCollection.find(query)
-      const  stockItem = await cursor.toArray()
-      res.send(stockItem)
+    app.get("/inventoryItems"),async (req, res) => {
+        const email = req.query.email;
+        const query = { email: email };
+        const cursor = inventoryItemsCollection.find(query);
+        const stockItem = await cursor.toArray();
+        res.send(stockItem);
+      };
 
-    }
-
-
-
-    // Update Inventory items 
-    app.put('/inventoryItems/:id', async (req, res) =>{
-
+    // Update Inventory items
+    app.put("/inventoryItems/:id", async (req, res) => {
       const id = req.params.id;
-      const updateItem = req.body
-      const filter = {_id: ObjectId(id)}
-      const options = {upsert: true}
+      const updateItem = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
 
       const updateItemFieldsDoc = {
-
-        $set:{
-          quantity:updateItem.quantity ,
+        $set: {
+          quantity: updateItem.quantity,
           price: updateItem.price,
-          
-        }
-      }
+        },
+      };
 
-      const result = await inventoryItemsCollection.updateOne(filter, updateItemFieldsDoc, options)
+      const result = await inventoryItemsCollection.updateOne(
+        filter,
+        updateItemFieldsDoc,
+        options
+      );
 
-      res.send(result)
-
-
-    })
-    
+      res.send(result);
+    });
 
     //Delete
 
-    app.delete('/inventoryItems/:id', async(req, res)=>{
+    app.delete("/inventoryItems/:id", async (req, res) => {
       const id = req.params.id;
-      const  query = {_id: ObjectId(id)  };
+      const query = { _id: ObjectId(id) };
       const result = await inventoryItemsCollection.deleteOne(query);
-      res.send(result)
-
+      res.send(result);
     });
 
     //Auth
-    app.post('/login' ,  async (req,res)=>{
-      const user =req.body
-      const accessToken= jwt.sign(user, process.env.ACCESS_TOKEN_SEC, {
-
-        expiresIn: '1d'
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SEC, {
+        expiresIn: "1d",
       });
       res.send(accessToken);
-    })
-
+    });
 
     //All blogs
-    app.get('/blogs', async(req,res)=>{
-
-      const query ={}
-      const cursor=blogsCollection.find(query)
-      const blogs= await cursor.toArray();
-      res.send(blogs)
-    })
+    app.get("/blogs", async (req, res) => {
+      const query = {};
+      const cursor = blogsCollection.find(query);
+      const blogs = await cursor.toArray();
+      res.send(blogs);
+    });
 
     // for 1 single blog
-    app.get('/blogs/:id', async(req,res)=>{
-      const id =req.params.id
-      const query ={_id:ObjectId(id)}
-      const blog = await blogsCollection.findOne(query)
-      res.send(blog)
-    })
-    
+    app.get("/blogs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const blog = await blogsCollection.findOne(query);
+      res.send(blog);
+    });
 
     /// for pagination
-    app.get('/inventoryImtesCount', async(req,res)=>{
-
-        const query= {}
-        const cursor =inventoryItemsCollection.find(query)
-        const count = await cursor.count()
-        res.send({count})
-    }) 
-
-   
-
+    app.get("/inventoryImtesCount", async (req, res) => {
+      const query = {};
+      const cursor = inventoryItemsCollection.find(query);
+      const count = await cursor.count();
+      res.send({ count });
+    });
   } finally {
-
-
   }
 }
 run().catch(console.dir);
@@ -152,4 +142,4 @@ app.get("/hero", (req, res) => {
 
 app.listen(port, () => {
   console.log("assignmetn server work to port ", port);
-})
+});
